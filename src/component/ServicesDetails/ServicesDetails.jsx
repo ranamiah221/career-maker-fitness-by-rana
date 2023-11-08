434ca18d0c7bb924e8597f8f999ee235d51b1387
ranamiah221 <ranarasul21@gmail.com>
2023-11-08 16:24:42 +0600

    added dynamic view details btn and private services details page

--- src/component/ServicesDetails/ServicesDetails.jsx
+++ src/component/ServicesDetails/ServicesDetails.jsx
@@ -1,10 +1,30 @@
+import { useLoaderData } from "react-router-dom";
 
 
 const ServicesDetails = () => {
+    const service = useLoaderData();
+    const { serviceImage, serviceName, serviceDescription, serviceProviderImage, serviceProviderName, servicePrice}=service;
     return (
-        <div>
-            
-        </div>
+       
+         <div className="card w-full card-side bg-base-100 shadow-2xl p-8 ">
+              <div className="w-1/2">
+                   <img className="rounded-lg w-full h-[400px]" src={serviceProviderImage} alt="Movie"/>
+              </div>
+            <div className="card-body w-1/2">
+                  <h2 className="text-4xl font-bold text-amber-400">{serviceProviderName}</h2>
+                  <p className="text-base font-medium text-gray-500">{serviceDescription}</p>
+                  <p className="text-base font-medium text-black"> Price :{servicePrice}</p>
+                  <p className="text-base font-medium text-black">Address : West Shewrapara, Mirpur 10. </p>
+                
+                   <div className="card-actions justify-end">
+                          <button className="btn btn-primary">Add</button>
+                          <button className="btn btn-primary">Delete</button>
+                          <button className="btn btn-primary">Update</button>
+                   </div> 
+          </div>
+
+      </div>
+      
     );
 };
 
